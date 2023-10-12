5cf0928c4a5659da0a7f70c1a37110bd4b2a0f86
eamato <veaceslav.buzdugan@gmail.com>
2023-10-12 22:15:35 +0300

    dialogs fixes

--- app/src/main/java/eamato/funn/r6companion/ui/dialogs/DialogDefaultPopupManager.kt
+++ app/src/main/java/eamato/funn/r6companion/ui/dialogs/DialogDefaultPopupManager.kt
@@ -6,54 +6,13 @@ import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import eamato.funn.r6companion.core.extenstions.isLandscape
 
-class DialogDefaultPopupManager(private val lifecycleOwner: LifecycleOwner) {
-
-    private var dialog: IDialogDefault? = null
-
-    private var observer: Observer? = null
-
-    init {
-        if (lifecycleOwner.lifecycle.currentState == Lifecycle.State.DESTROYED) {
-            dialog?.dismiss()
-            dialog = null
-            observer?.run { lifecycleOwner.lifecycle.removeObserver(this) }
-            observer = null
-        }
-
-        observer?.run { lifecycleOwner.lifecycle.removeObserver(this) }
-        val observer = Observer()
-        lifecycleOwner.lifecycle.addObserver(observer)
-        this.observer = observer
-    }
+object DialogDefaultPopupManager {
 
     fun create(context: Context): IDialogDefault {
         if (context.isLandscape()) {
-            dialog?.dismiss()
-            dialog = null
-
-            return DialogDefaultAppPopupSide(context).also { dialog = it }
+            return DialogDefaultAppPopupSide(context)
         }
 
-        dialog?.dismiss()
-        dialog = null
-
-        return DialogDefaultAppPopup().also { dialog = it }
-    }
-
-    fun dismiss() {
-        dialog?.dismiss()
-    }
-
-    private inner class Observer : LifecycleEventObserver {
-
-        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-            if (event == Lifecycle.Event.ON_DESTROY) {
-                dialog?.dismiss()
-                dialog = null
-
-                observer?.run { lifecycleOwner.lifecycle.removeObserver(this) }
-                observer = null
-            }
-        }
+        return DialogDefaultAppPopup()
     }
 }
