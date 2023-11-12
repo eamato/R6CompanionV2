314e07ed0d28d9250cad490d12618d54d66608a9
eamato <veaceslav.buzdugan@gmail.com>
2023-11-12 20:46:36 +0200

    * operators argument added to roll result screen * operator id argument added to operator details screen * roll result screen uses deep link functionality to navigate to operator details screen to show details about winner operator in roulette

--- app/src/main/java/eamato/funn/r6companion/ui/fragments/roulette/FragmentRouletteResult.kt
+++ app/src/main/java/eamato/funn/r6companion/ui/fragments/roulette/FragmentRouletteResult.kt
@@ -1,6 +1,10 @@
 package eamato.funn.r6companion.ui.fragments.roulette
 
+import android.net.Uri
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import eamato.funn.r6companion.databinding.FragmentRouletteResultBinding
@@ -10,4 +14,14 @@ import eamato.funn.r6companion.ui.fragments.ABaseFragment
 class FragmentRouletteResult : ABaseFragment<FragmentRouletteResultBinding>() {
 
     override val bindingInitializer: (LayoutInflater) -> ViewBinding = FragmentRouletteResultBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.hui?.setOnClickListener {
+            val oper = FragmentRouletteResultArgs.fromBundle(requireArguments()).rollCandidates.first()
+            val uri = Uri.parse("android-app://eamato.funn.r6companion/operator/details/${oper.id}")
+            findNavController().navigate(uri)
+        }
+    }
 }
