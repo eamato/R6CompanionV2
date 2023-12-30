806850012e8638ad93937eed9ae2155132460415
eamato <veaceslav.buzdugan@gmail.com>
2023-12-30 12:09:01 +0200

    logger build type check added

--- app/src/main/java/eamato/funn/r6companion/core/utils/logger/ILogger.kt
+++ app/src/main/java/eamato/funn/r6companion/core/utils/logger/ILogger.kt
@@ -1,19 +1,26 @@
 package eamato.funn.r6companion.core.utils.logger
 
 import android.util.Log
+import eamato.funn.r6companion.BuildConfig
 import eamato.funn.r6companion.core.LOGGER_TAG
 
 interface ILogger {
 
     fun d(msg: Message? = null, err: Throwable? = null) {
-        Log.d(LOGGER_TAG, msg?.string(), err)
+        if (BuildConfig.DEBUG) {
+            Log.d(LOGGER_TAG, msg?.string(), err)
+        }
     }
 
     fun i(msg: Message? = null, err: Throwable? = null) {
-        Log.i(LOGGER_TAG, msg?.string(), err)
+        if (BuildConfig.DEBUG) {
+            Log.i(LOGGER_TAG, msg?.string(), err)
+        }
     }
 
     fun e(msg: Message? = null, err: Throwable? = null) {
-        Log.e(LOGGER_TAG, msg?.string(), err)
+        if (BuildConfig.DEBUG) {
+            Log.e(LOGGER_TAG, msg?.string(), err)
+        }
     }
 }
